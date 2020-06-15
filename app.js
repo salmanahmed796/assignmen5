4cb8de54665b245b447be5cc2a5f99e7e0654d88
salmanahmed796 <m.salmanahmed796@gmail.com>
2020-06-16 01:34:06 +0500

    Now ready to Assignment 5

---
+++ app.js
@@ -0,0 +1,170 @@
+// // chapter # 01
+// // Question # 01
+// alert("Hello! You're Welcome");
+
+// // Question # 02
+// alert("Error! please enter a valid password.");
+
+// // Question # 03
+// alert("Welcome to JS Land... \n Happy codding");
+
+// // Question # 03
+// alert("Welcome to JS Land...");
+
+// alert("Happy Codding!");
+
+// // Question # 04
+// alert("Hello... I can run JS through my web browser's console");
+
+
+// // chapter # 02
+// // Question # 01
+// var username = "johne Doe";
+// alert(username)
+
+// var age = "15 Years old";
+// alert(age)
+
+// var certified = "Certified Mobile Application Development";
+// alert(certified)
+
+// // Question # 02
+// var pizza = "PIZZA\nPIZZ\nPIZ\nPI\nP";
+// alert(pizza)
+
+// // Question # 02
+// var email = "My email address is example@example.com";
+// alert(email)
+
+// // Question # 03
+// var learn = "I am trying to learn from the Book a Smarter Way to learn JavaScript";
+// alert(learn)
+
+// // Question # 04
+// var write = "Yah! I can write HTML content through JavaScript";
+// document.write(write)
+
+// // Question # 05
+// var draw = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+// alert(draw)
+
+// // Chapter # 03
+// // Question # 01
+// var age = "I am 15 years old";
+// alert(age)
+
+// // Question # 02
+// var visit = "You have visited this site 14 times";
+// alert(visit)
+
+// // Question # 03
+// var birth = "My birth year is 1990 <br> Data type of my declared variable is number";
+// document.write(birth)
+
+// // Question # 04
+// var order = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store";
+// document.write(order)
+
+// // Chapter # 04
+// // Question # 01
+// var rules = "<h3>Rules for naming JS variables</h3> <br>";
+// var para = "variable names can only contain, numbers, $ and___. For example: $my__1stVariable <br> Variable must begin with a letter,$ or___. For example: $name,___ name or name <br> Variable names are case sensitive <br> Variable names should not be JS Keywords";
+// document.write(rules + para)
+
+
+// // chapter # 05
+// // Question # 01
+// var sum = "sum of 3 and 5 is 8"
+// document.write(sum)
+
+// // Question # 02
+// var num ="5";
+// num++;
+// num = num + 7;
+// num--;
+// num = num % 3;
+
+// document.write("Value after variable declaration is undefined <br>Initial value : 5<br>Value after increment is: 6<br> Value after addition is :  13<br>Value after decrement is: 12<br>The remainder is :"+num);
+
+// // Question # 03
+// var cost = "600";
+// cost = cost * 5;
+// document.write("Total cost to buy 5 tickets to a movie is 3000PKR")
+
+// // Question # 04
+// var table = "Table of 4 </br>";
+// document.write(table)
+// for (var i = 1; i <= 10; i++){
+//     document.write("4" + "x" + i + "=" + 4*i + "<br>");
+// }
+
+// // Question # 05
+// function cToF(celsius) 
+// { 
+//   var cTemp = celsius;
+//   var cToFahr = cTemp * 9 / 5 + 32;
+//   var message = cTemp+'\xB0C is ' + cToFahr + ' \xB0F. <br>' ;
+//     document.write(message);
+// }
+
+// function fToC(fahrenheit) 
+// {
+//   var fTemp = fahrenheit;
+//   var fToCel = (fTemp - 32) * 5 / 9;
+//   var message = fTemp+'\xB0F is ' + fToCel + '\xB0C.';
+//   document.write(message);
+// } 
+// cToF(25);
+// fToC(70);
+
+// // Question # 06
+// var shop = "<h1> Shopping Cart </h1>"
+// var cart = "<p>price of item 1 is 650 </br> Quantity of item 1 is 3 </br> Price of item 2 is 100 </br> Quantity of item 2 is 7 </br> Shipping Charges 100 </br> </br> Total cost of your order is 2750";
+// document.write(shop+cart)
+
+
+// // Question # 7
+// var Marks = 980;
+// var obtain = 804;
+// Marks = obtain / Marks * 100;
+// document.write(Marks)
+
+// var sheet = " <h1>Marks Sheets</h1> </br>";
+// var a = "Total marks: 980 </br>";
+// var b = "Marks obtained: 804 </br>";
+// var p = "Percentage: 82.0408163265306 </br>";
+// document.write(sheet + a + b + p)
+
+    // // Question # 8
+    // var dollar = 104.80;
+    // convertD = dollar * 10;
+    // var riyal = 28;
+    // convertR = riyal * 25;
+    // document.write(convertD + convertR)
+
+    // var a = "<h1> Currency in PKR</h1> </br>";
+    // var b = "Total currency in PKR: 1748"
+    // document.write(a+b)
+
+    // // Question # 9
+    // var year = 2016;
+    // var birth = 1992;
+    // age = year - birth;
+    // document.write(age)
+
+
+    // var a = "<h1>Age Calculator</h1></br>";
+    // var b = "Current year:2016</br> Birth year:1992</br> Your age:24";
+    // document.write(a + b)
+
+    // // Question # 11
+    // var a = 15;
+    // b = 65;
+    // c= 3;
+    // z = (b - a) * c;
+    // document.write(z)
+
+    // var a = "<h1> The Lifetime Supply Calculator</h1> </br>";
+    // var b = "Favourite snack: chocolate chip </br> Current age: 15 </br> Extimeted maximum age: 65</br> Amount of snack per day: 3</br> You will need 150 chocolate chip to last you until the ripe old age of 65";
+    // document.write(a + b)
+
